eed699c82a07a6d5341147cad5ab0dd62f5b7300
Aditya Mohan <mohanadityajha@gmail.com>
2025-04-22 13:22:39 +0530

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "Aditya@google.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2  // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+Prefer not use Var 
+because of issue in block scope and functional scope
+
+*/
