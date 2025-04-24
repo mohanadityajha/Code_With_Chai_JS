fb94b77454de384cc9e59c5ad9fd1f9752fdc729
Aditya Mohan <mohanadityajha@gmail.com>
2025-04-24 21:23:19 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+//primitive's Data Type (reference by value)
+// 7 Types: String, Number, Boolean, null, undefined,Symbol,BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId)
+
+const bigNumber = 34567890231n
+
+// Non-Primitive (Reference By Call )
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", naagraj, "doga"]
+let myObj = {
+  name: "hitesh",
+  age:22
+}
+
+const myFunction = function () { 
+console.log("Hello World")
+
+}
