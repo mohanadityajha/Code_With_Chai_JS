420e17f23d9b791ac3bb5c3f3fee2d43d3016338
Aditya Mohan <mohanadityajha@gmail.com>
2025-04-24 15:05:29 +0530

    DataType

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+"Use strict"; // treat all js code as newer version
+
+// alert (3+3) // we are using node js, not browser
+
+console.log("Hitesh")
+
+
+let name = ("hitesh")
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+//null => standalone value
+// undefined => we can intialzed but not declare
+// symbol => unique
+
+
+// object
+console.log(typeof "Hitesh")
+console.log(typeof undefined); return type = undefined
+console.log(typeof null) // return type = object
