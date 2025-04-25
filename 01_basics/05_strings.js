3ce4a513fa1e8032e18388de66ee1af8181f47a8
Aditya Mohan <mohanadityajha@gmail.com>
2025-04-25 19:52:58 +0530

    String-Methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "hitesh"
+const repoCount = 50
+// console.log(name + repoCount + " value") // its is outdated using + symbol
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)  // This is correct way 
+
+const gameName = new String("hitesh-hc-com")
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = " hitesh   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://hitesh.com/hitesh20%choudhary"
+console.log(url.replace("20%", "-"))
+console.log(url.includes("hitesh"))
+console.log(url.includes("Aditya"))
+console.log(gameName.split("-"))
