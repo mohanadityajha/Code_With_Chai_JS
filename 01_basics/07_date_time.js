b89fff6ba12dee6d4f753e49963b6a85f3e51d51
Aditya Mohan <mohanadityajha@gmail.com>
2025-05-05 22:17:01 +0530

    Array

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,25 @@
+//let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toJSON())
+// console.log(typeof myDate)
+
+
+
+// let createdDate = new Date(2023, 0, 23, 5, 4);
+// let createdDate = new Date("2023-01-23");
+let createdDate = new Date("02-04-2023");
+// console.log(createdDate.toDateString());
+// console.log(createdDate.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+// console.log(createdDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth()+ 1)
+console.log(newDate.getDate())
